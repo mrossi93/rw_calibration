c144149782ed78ed62c305503eeb374940b4d730
Matteo Rossi <rossimatteo1993@gmail.com>
2020-03-28 19:46:52 +0100

    Added calibration routine and CLI

--- rw_calibration/cli.py
+++ rw_calibration/cli.py
@@ -1,14 +1,36 @@
 """Console script for rw_calibration."""
 import sys
 import click
+from rw_calibration import calibrate
 
 
 @click.command()
-def main(args=None):
+@click.option("--wfile", "-w", help="Path to World Coordinates File")
+@click.option("--rfile", "-r", help="Path to Robot Coordinates File")
+def main(wfile, rfile):
     """Console script for rw_calibration."""
-    click.echo("Replace this message by putting your code into "
-               "rw_calibration.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
+    if wfile is None:
+        click.echo(
+            click.style(
+                "Error: please indicate the path to World Coordinates File", fg="red"
+            )
+        )
+        return 1
+    if rfile is None:
+        click.echo(
+            click.style(
+                "Error: please indicate the path to Robot Coordinates File", fg="red"
+            )
+        )
+        return 1
+
+    results = calibrate(wfile, rfile)
+
+    click.echo("\nRototranslator:\n{}".format(results["Rototranslator"]))
+    click.echo("\nError:\t\t\tx\ty\tz")
+    click.echo("   Mean:\t{}".format(results["Error Mean"]))
+    click.echo("   Std Dev:\t{}".format(results["Error Std Dev"]))
+
     return 0
 
 
